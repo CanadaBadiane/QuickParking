eddf5410c892bf9534a96edfe1a0572983047fa8
Canada Badiane <canada1555@hotmail.com>
2026-03-27 22:07:44 -0400

    refactor: suppression de la gestion d’erreur locale et nettoyage du localStorage

--- app/pre-paiements/page.tsx
+++ app/pre-paiements/page.tsx
@@ -13,7 +13,6 @@ export default function PrePaiementPage() {
   const [parkingspotId, setParkingspotId] = useState("");
   const [duration, setDuration] = useState("");
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
   const { user } = useUser();
   const [role, setRole] = useState<string>("");
   const [clerkId, setClerkId] = useState("");
@@ -46,7 +45,6 @@ export default function PrePaiementPage() {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     setLoading(true);
-    setError("");
     try {
       const token = await getToken();
       const body: any = {
@@ -69,7 +67,6 @@ export default function PrePaiementPage() {
         toast.error(data?.error || data?.message || "Erreur paiement");
       // Stocke l'ID et le montant dans le localStorage pour la page paiement
       localStorage.setItem("selectedParkingSpotId", parkingspotId);
-      localStorage.removeItem("selectedParkingSpotId");
       if (data.paiement && data.paiement.amount) {
         localStorage.setItem("selectedAmount", String(data.paiement.amount));
       }
@@ -150,7 +147,6 @@ export default function PrePaiementPage() {
                   min={10}
                 />
               </div>
-              {error && <div className="text-red-500">{error}</div>}
               <button
                 type="submit"
                 className="bg-blue-600 text-white px-4 py-2 rounded cursor-pointer hover:bg-blue-700 disabled:opacity-50"
